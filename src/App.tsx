480fa681ddf74da139515e8c804e9d20647b70a3
ArcOmotola <ogunsola40@gmail.com>
2022-04-15 22:07:38 +0100

    defined cartItemType

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
+//components
+import { Drawer, LinearProgress, Badge, Grid } from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+//Styles
+import { Wrapper } from "./App.styles";
+
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+
+
+const getProducts = async () => {
+  await (await fetch('https://fakestoreapi.com/products')).json();         //doubles await because fetching from api, and converting to JSON are both asynchronous
+}
+
+const App = () => {
   return (
     <div className="App">
       TEST
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
+  // </React.StrictMode>
 );
 
